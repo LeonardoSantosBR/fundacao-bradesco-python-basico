d02b77153b32daf3382d38141f00d8e10f1803b1
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-02-18 23:38:23 -0300

    init: projeto aprender python

---
+++ variables.py
@@ -0,0 +1,15 @@
+#integer
+pi = 3.14
+raio= 5
+area= pi*(raio*raio)
+print(area)
+
+#string
+name= "leonardo"
+actualJob= "developer"
+print(name+actualJob)
+
+#boolean
+isMale= True
+isFemale= False
+print(isMale, isFemale)
