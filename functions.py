513881d8bc4651f69144a3c1df41a7c003b14fac
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-02-21 10:22:54 -0300

    feat: functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def firstFunction ():
+    print("executing first function")
+    
+    
+def secondFunction ():
+    return "Hello world"
+
+
+firstFunction()
+text = secondFunction()
+print("result: ",text)
